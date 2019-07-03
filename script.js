e91f3410cf806adc1678a0b2ba991a000374c1e5
Robin <robin-wagner@web.de>
2019-07-03 21:20:59 +0200

    Create initial version

---
+++ script.js
@@ -0,0 +1,11 @@
+let dimensions = 16;
+
+let container = document.querySelector('#container');
+
+for (let squareRow = 0; squareRow < dimensions; squareRow++) {
+  for (let squareColumn = 0; squareColumn < dimensions; squareColumn++) {
+    let div = document.createElement('div');
+    div.setAttribute('id', 'square');
+    container.appendChild(div);
+  }
+}
